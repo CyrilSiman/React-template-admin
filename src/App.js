75c504262b43d89813b90493ebf46e4a6eb3b8cc
Cyril Siman <csiman.phone@gmail.com>
2019-12-21 19:19:50 +0100

    Init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import constants from 'ROOT/services/constants'
+import routes from 'ROOT/routes'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AuthenticatedRouter from 'ROOT/AuthenticatedRouter'
+
+import AppInitScene from 'ROOT/scenes/AppInit'
+import LoginScene from 'ROOT/scenes/Auth/scenes/Login'
+import LostPasswordScene from 'ROOT/scenes/Auth/scenes/LostPassword'
+import { useQuery } from '@apollo/react-hooks'
+import { connected } from 'ROOT/services/graphql/localState.graphql'
+import {appConfiguredQuery} from 'ROOT/services/graphql/appConfig.graphql'
+
+function App () {
+
+    const {data,loading} = useQuery(appConfiguredQuery)
+
+    if(loading) {
+        return <div>Loading</div>
+    }
+
+    console.log(data)
+
+    if(!loading && data && !data.appConfigured) {
+        return <Router><AppInitScene/></Router>
+    }
+
+    return (
+        <Router>
+            <Switch>
+                <Route component={LoginScene} path={routes.LOGIN} />
+                <Route component={LostPasswordScene} path={routes.LOST_PASSWORD} />
+                <PrivateRoute component={AuthenticatedRouter} path={routes.PRIVATE_DEFAULT} />
+                <Route component={NoMatch} />
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+function NoMatch ({ ...rest }) {
+
+    const { data } = useQuery(connected)
+    return (
+        <Route
+            {...rest}
+            render={props => {
+                return (data.isLoggedIn ? (
+                    <Redirect to={{ pathname: routes.PRIVATE_DASHBOARD, state: { from: props.location } }} />
+                ) : (
+                    <Redirect to={{ pathname: routes.LOGIN, state: { from: props.location } }} />
+                ))
+            }
+            }
+        />
+    )
+}
+
+function PrivateRoute ({ component: Component, ...rest }) {
+    const { data } = useQuery(connected)
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                data.isLoggedIn ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={{ pathname: routes.LOGIN, state: { from: props.location } }} />
+                )
+            }
+        />
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Suspense} from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AppApolloClient from 'ROOT/services/AppApolloClient'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {MuiThemeProvider} from '@material-ui/core'
+import {defaultTheme} from 'ROOT/themes'
+import {MuiPickersUtilsProvider} from '@material-ui/pickers'
+import frLocale from 'date-fns/locale/fr'
+import DateFnsUtils from '@date-io/date-fns'
+import {SnackbarProvider} from 'notistack'
+
+import './configI18n'
+
+import { ApolloProvider } from '@apollo/react-hooks'
+
+ReactDOM.render(
+    <Suspense fallback="loading">
+        <ApolloProvider client={AppApolloClient}>
+            <MuiThemeProvider theme={defaultTheme}>
+                <MuiPickersUtilsProvider utils={DateFnsUtils} locale={frLocale}>
+                    <CssBaseline/>
+                    <SnackbarProvider dense maxSnack={3} autoHideDuration={3000}
+                                      anchorOrigin={{
+                                          vertical: 'bottom',
+                                          horizontal: 'right',
+                                      }}>
+                            <App/>
+                    </SnackbarProvider>
+                </MuiPickersUtilsProvider>
+            </MuiThemeProvider>
+        </ApolloProvider>
+    </Suspense>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
